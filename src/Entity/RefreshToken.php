90f0af201664aa80d863ab03b4fc6b21e5f07ef3
Loïc <loic.papazian@outlook.fr>
2022-11-15 10:35:42 +0100

    add RefreshToken and setup into api

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -5,10 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
-/**
- * @ORM\Entity
- * @ORM\Table("refresh_tokens")
- */
+#[ORM\Entity()]
 class RefreshToken extends BaseRefreshToken
 {
 }
